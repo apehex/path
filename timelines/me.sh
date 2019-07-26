f94bbd3f0ca683076894d4c21a2765bba2a5a21b
David Mougeolle <david.mougeolle@moodule.net>
2019-07-26 12:51:01 +0200

    Add placeholder files for future dev

---
+++ timelines/me.sh
@@ -0,0 +1,26 @@
+me
+-- training
+-- work
+-- hobbies
+-- failures
+-- social ?
+
+# prepa
+
+# ecl
+
+# pi
+
+# c3 / ecs / etc
+
+# mountaineering
+
+# yoga
+
+# diving
+
+# tutoring
+
+# bike delivery
+
+# open source
