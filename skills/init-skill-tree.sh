d7194caa0939e4b409bfb732f923168d5ed9a9ad
David Mougeolle <david.mougeolle@moodule.net>
2019-08-12 22:09:29 +0200

    Add basic (computer) science skills

--- skills/init-skill-tree.sh
+++ skills/init-skill-tree.sh
@@ -50,7 +50,53 @@ mkdir -p ./technical/maths/modeling/regression
 mkdir -p ./technical/maths/statistics
 mkdir -p ./technical/maths/trigonometrics
 
+# physics
+mkdir -p ./technical/physics/electronics/
+mkdir -p ./technical/physics/materials/
+mkdir -p ./technical/physics/mechanics/solid/
+mkdir -p ./technical/physics/mechanics/fluids/
+mkdir -p ./technical/physics/quantic/
+mkdir -p ./technical/physics/thermodynamics/
+
+# chemistry
+mkdir -p ./technical/chemistry/
+
+# software-engineering
+mkdir -p ./technical/computing/software-engineering/
+
+# cyber-security
+mkdir -p ./technical/computing/cyber-security/blue/cryptography
+mkdir -p ./technical/computing/cyber-security/blue/endpoint/authentication
+mkdir -p ./technical/computing/cyber-security/blue/endpoint/configuration
+mkdir -p ./technical/computing/cyber-security/blue/endpoint/rights
+mkdir -p ./technical/computing/cyber-security/blue/intelligence/threat-cataloging
+mkdir -p ./technical/computing/cyber-security/blue/network/firewall
+mkdir -p ./technical/computing/cyber-security/blue/network/monitoring
+mkdir -p ./technical/computing/cyber-security/blue/network/scanning
+mkdir -p ./technical/computing/cyber-security/blue/social/anti-spam
+
 # pentesting
-mkdir -p ./technical/computer-science/security/blue
-mkdir -p ./technical/computer-science/security/red
-mkdir -p ./technical/security/computer-science/blue
+mkdir -p ./technical/computing/cyber-security/red/cryptography/cracking # bruteforce, hash
+mkdir -p ./technical/computing/cyber-security/red/exfiltration/
+mkdir -p ./technical/computing/cyber-security/red/hardware/keylogging
+mkdir -p ./technical/computing/cyber-security/red/hardware/recording
+mkdir -p ./technical/computing/cyber-security/red/intelligence/osint
+mkdir -p ./technical/computing/cyber-security/red/intelligence/pattern-identification # machine learning
+mkdir -p ./technical/computing/cyber-security/red/malware/reverse-engineering
+mkdir -p ./technical/computing/cyber-security/red/malware/virus
+mkdir -p ./technical/computing/cyber-security/red/mitm/fake-ap
+mkdir -p ./technical/computing/cyber-security/red/mitm/fake-dns
+mkdir -p ./technical/computing/cyber-security/red/traffic/replay # and editing
+mkdir -p ./technical/computing/cyber-security/red/traffic/sniffing # dump
+mkdir -p ./technical/computing/cyber-security/red/vulnerability-scanning/network
+mkdir -p ./technical/computing/cyber-security/red/side-channels/electromagnetic-analysis
+mkdir -p ./technical/computing/cyber-security/red/side-channels/sound-analysis
+mkdir -p ./technical/computing/cyber-security/red/social-engineering/phishing
+
+
+# risk-management
+mkdir -p ./technical/risk-management/forensic
+mkdir -p ./technical/risk-management/monitoring # reacting in time => vs fire, bruteforce, etc
+mkdir -p ./technical/risk-management/pentesting
+mkdir -p ./technical/risk-management/scanning
+mkdir -p ./technical/risk-management/trapping
