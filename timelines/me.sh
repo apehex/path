8002b2a5ac11b29e3caa76aaac6d18187e15e056
David Mougeolle <david.mougeolle@moodule.net>
2019-09-12 09:20:43 +0200

    Tag the web templates dev of 2019

--- timelines/me.sh
+++ timelines/me.sh
@@ -1,9 +1,135 @@
-me
--- training
--- work
--- hobbies
--- failures
--- social ?
+#!/bin/bash
+
+# TIMELINE START #
+##################
+
+git checkout master
+touch timeline-me-root
+git add .
+git commit --date="2005-09-01" --message="Starting my studies"
+
+# TRAININGS #
+#############
+
+git checkout master
+git checkout -b training
+echo "|" >> ./technical/computing/languages/scientific/octave
+git add .
+git commit --date="2016-07-01T00:00:00" --message="Translate my knowledge of Matlab to Octave"
+git tag "Completed 'ML' on Coursera" HEAD
+
+# WORK #
+########
+
+git checkout master
+git checkout -b work
+git checkout -b web-development
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-02-01T00:00:00" --message="Made my first static website template on Jekyll, Stellar"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-03-01T00:00:00" --message="Made another static website template on Jekyll, Twenty"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-04-01T00:00:00" --message="Made another static website template on Jekyll, Overflow"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-05-01T00:00:00" --message="Made another static website template on Jekyll, Drop"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-06-01T00:00:00" --message="Made another static website template on Jekyll, Consulting"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-07-01T00:00:00" --message="Made a horizontal scrolling static website template on Jekyll, Parallelism"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+git add .
+git commit --date="2019-08-01T00:00:00" --message="Made another horizontal scrolling static website template on Jekyll, Ethereal"
+
+echo "|" >> ./technical/computing/data-processing/image/gimp
+echo "|" >> ./technical/computing/data-processing/image/inkscape
+echo "|" >> ./technical/computing/languages/data-driven/svg
+echo "|" >> ./technical/computing/languages/data-driven/yaml
+echo "|" >> ./technical/computing/languages/declarative/css
+echo "|" >> ./technical/computing/languages/declarative/html
+echo "|" >> ./technical/computing/languages/object-oriented/ruby
+echo "|" >> ./technical/computing/languages/scripting/js
+echo "|" >> ./mental/awareness/self
+git add .
+git commit --date="2019-09-01T00:00:00" --message="Used my templates for a business site, CDM"
+
+# FAILURES #
+############
+
+git checkout master
+git checkout -b failures
+
+# PROJECTS #
+############
+
+git checkout master
+git checkout -b projects
+
+# escalade / yoga / apnée / plongée / crunching / 
+
+# learning to learn
+
+# missions / objectives / skills / issues / timing / 
 
 # prepa
 
