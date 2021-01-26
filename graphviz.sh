70528a914ef7a83afd839a4577670f138723139a
moodule <moodule@protonmail.com>
2021-01-26 11:59:08 +0100

    Test pretty log formats from the web

--- graphviz.sh
+++ graphviz.sh
@@ -1,4 +1,6 @@
 git log --graph --all --decorate --oneline
 git log --graph --full-history --all --pretty=format:"%h%x09%d%x20%s"
 git log --graph --full-history --all --color \
-        --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+--pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
+git log --graph --abbrev-commit --decorate \
+--format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'
